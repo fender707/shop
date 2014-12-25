72948f947aa0c2f5dfb0b6409f2d1b5ef07435d1
Dmitriy Panasiuk <dmitriy.panasyuk@gmail.com>
2014-12-25 10:34:31 +0200

    added tests for factories and associations

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Category, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Category do
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:category)). to be_valid
+  end
+
+  it {should have_many(:products)}
 end
