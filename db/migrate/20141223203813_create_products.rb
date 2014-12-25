8a1a7706143027bc651f46a00358736a3d05c19d
Dmitriy Panasiuk <dmitriy.panasyuk@gmail.com>
2014-12-25 10:20:45 +0200

    changed product description field type to text

--- db/migrate/20141223203813_create_products.rb
+++ db/migrate/20141223203813_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :title
-      t.string :description
+      t.text :description
       t.integer :count
       t.decimal :price
       t.belongs_to :category, index: true
