e9889e0f3962bceffec868a581f3b2c6d30ac953
Dmitriy Panasiuk <dmitriy.panasyuk@gmail.com>
2014-12-29 15:26:04 +0200

    devise authentication configuration

--- db/schema.rb
+++ db/schema.rb
@@ -11,27 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141223213519) do
+ActiveRecord::Schema.define(version: 20141229114132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "carts", force: true do |t|
-    t.datetime "created_at", :nul => false
-    t.datetime "updated_at", :nul => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "categories", force: true do |t|
     t.string   "name"
     t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "line_items", force: true do |t|
     t.integer  "product_id"
     t.integer  "cart_id"
     t.integer  "order_id"
-    t.datetime "created_at", :nul => false
-    t.datetime "updated_at", :nul => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id", using: :btree
@@ -42,8 +44,8 @@ ActiveRecord::Schema.define(version: 20141223213519) do
     t.string   "name"
     t.string   "address"
     t.string   "email"
-    t.datetime "created_at", :nul => false
-    t.datetime "updated_at", :nul => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "products", force: true do |t|
@@ -52,8 +54,8 @@ ActiveRecord::Schema.define(version: 20141223213519) do
     t.integer  "count"
     t.decimal  "price"
     t.integer  "category_id"
-    t.datetime "created_at", :nul => false
-    t.datetime "updated_at", :nul => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
@@ -61,6 +63,26 @@ ActiveRecord::Schema.define(version: 20141223213519) do
   create_table "sellers", force: true do |t|
     t.string   "name"
     t.string   "password"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
