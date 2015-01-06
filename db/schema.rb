9a882f1e5c3548af9b4316bc8b05560f808487bd
Slava <fender707@rambler.ru>
2015-01-06 23:02:45 +0200

    Added column image_url to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141229114132) do
+ActiveRecord::Schema.define(version: 20150106204358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20141229114132) do
     t.integer  "category_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_url"
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
