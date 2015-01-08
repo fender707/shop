99c06b9b4b9943d73989177d3eb817a0e55a53f3
Slava <fender707@rambler.ru>
2015-01-08 12:38:26 +0200

    Added tabulation to controller products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,40 +22,40 @@ class ProductsController < ApplicationController
 # POST /reviews
 # POST /reviews.json
   def create
-   @product = Product.new(myparams)
-     respond_to do |format|
-if @product.save
-format.html { redirect_to @product, notice: 'Product was successfully created.' }
-format.json { render json: @product, status: :created, location: @product }
-else
-format.html { render action: "new" }
-format.json { render json: @product.errors, status: :unprocessable_entity }
-end
-end
+    @product = Product.new(myparams)
+      respond_to do |format|
+    if @product.save
+      format.html { redirect_to @product, notice: 'Product was successfully created.' }
+      format.json { render json: @product, status: :created, location: @product }
+    else
+      format.html { render action: "new" }
+      format.json { render json: @product.errors, status: :unprocessable_entity }
+    end
+                 end
   end
 # PUT /reviews/1
 # PUT /reviews/1.json
-def update
-@product = Product.find(params[:id])
-respond_to do |format|
-if @product.update_attributes(params[:product])
-format.html { redirect_to @product, notice: 'Product was successfully updated.' }
-format.json { head :no_content }
-else
-format.html { render action: "edit" }
-format.json { render json: @product.errors, status: :unprocessable_entity }
-end
-end
-end
+  def update
+    @product = Product.find(params[:id])
+    respond_to do |format|
+      if @product.update_attributes(params[:product])
+        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+               end
+  end
 # DELETE /reviews/1
 # DELETE /reviews/1.json
-def destroy
-@product = Product.find(params[:id])
-@product.destroy
-respond_to do |format|
-format.html { redirect_to products_url }
-format.json { head :no_content }
-end
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_url }
+      format.json { head :no_content }
+  end
 end
 
 
