16edf6ed6718cfb34e4ab2331aa5c0a5ceb8b870
Slava <fender707@rambler.ru>
2015-01-15 01:09:28 +0200

    Added views for orders, CSS

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,14 @@
 class OrdersController < ApplicationController
+
+  def new 
+    if @cart.line_items.empty?
+      redirect_to root_path, notice: "Your cart is empty"
+      return
+    end
+  
+    @order = Order.new
+    respond_to do |format|
+      format.html
+    end
+  end
 end
