63a2627d08f62b6f6a7702e27f2c4e65cd578274
Slava <fender707@rambler.ru>
2015-04-16 12:21:49 +0300

    Added functions for add line items to order and finish order session

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,26 @@
 class OrdersController < ApplicationController
 
+  def create 
+    @order = Order.new(order_params)
+    @order.add_line_items_from_cart(current_cart)
+      
+      respond_to do |format|
+        if @order.save
+	  Cart.destroy(session[:cart_id])
+	  session[:cart_id] = nil
+          format.html {redirect_to root_path, notice:
+	    'Thank you for your order.'}
+ 	  format.json {render json: @order, status: :created,
+	    location: @order }
+	else
+	  @cart = current_cart
+           format.html { render action: "new" }
+ 	   format.json { render json: @order.errors, status: :unprocessable_entity }
+	end
+      end
+  end  
+    
+
   def new 
     @cart = current_cart
     if @cart.line_items.empty?
@@ -11,5 +32,11 @@ class OrdersController < ApplicationController
     respond_to do |format|
       format.html
     end
+  end
+
+  private
+  
+  def order_params
+    params.require(:order).permit(:name, :address, :email)
   end
 end
