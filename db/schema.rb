9f5f3736a194548070dc1ab76bb5db48944727b1
Slava <fender707@rambler.ru>
2015-06-05 16:34:29 +0300

    Added Static google map to contacts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150429190412) do
+ActiveRecord::Schema.define(version: 20150528125921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20150429190412) do
   create_table "categories", force: true do |t|
     t.string "name"
     t.string "description"
+  end
+
+  create_table "contacts", force: true do |t|
+    t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "line_items", force: true do |t|
