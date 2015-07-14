09c528d83bd9935c7f213c86d5404100b11042ee
Slava <fender707@rambler.ru>
2015-07-14 16:24:56 +0300

    Send email in development mode

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,11 +1,11 @@
 class Notifier < ActionMailer::Base
-  default from: "myfirstshop@example.com"
+  default from: "Slava Sokalo <myfirstshop@example.com>"
   
 
-  def welcome(user)  
-    @user = user
-    mail(to: user.email,
-       subject: "[Signed up] Welcome #{user}")
+  def welcome(order)  
+    @order = order
+    mail(to: order.email,
+       subject: "[Signed up] Welcome #{order.name}")
   end 
 
 end
