341979bc21fbcfb40fc4c5c7fdc388369c6ffb45
Slava <fender707@rambler.ru>
2015-11-06 16:16:44 +0200

    Play with images

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
- Rails.application.config.assets.precompile += %w( bx_loader.gif controls.png *.css *.js *.mp3 orders/*.js )
+ Rails.application.config.assets.precompile += %w( bx_loader.gif controls.png *.css *.js *.mp3 orders/*.js images/main_page/*.png )
